6d4406eb3ef8451cd275496d6eeb7c7eea53c3d5
chensheng.zcs <chensheng.zcs@alipay.com>
2018-07-04 11:38:28 +0800

    初始化HttpClient和微信支付

---
+++ src/main/java/com/shinnlove/wechatpay/PoolingHttpClient.java
@@ -0,0 +1,17 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package com.shinnlove.wechatpay;
+
+/**
+ * 带有连接池的自定义HttpClient。
+ *
+ * @author shinnlove.jinsheng
+ * @version $Id: PoolingHttpClient.java, v 0.1 2018-07-04 上午11:31 shinnlove.jinsheng Exp $$
+ */
+public class PoolingHttpClient {
+
+
+
+}
