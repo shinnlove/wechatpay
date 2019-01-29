29264f9f4852bd016dc803cbc9a9997a10231998
chensheng.zcs <chensheng.zcs@alipay.com>
2019-01-29 16:58:15 +0800

    Https请求下载图片，用URLConnection去get直接得到SSL Connection Error。 用SSLSocketFactory来做加密通信Socket生成工厂可以正确下载。 如果是网页则直接转换成String，如果是图片则是二进制流，应该打开文件并且把Response接受流里的二进制数据读出来。

--- src/main/java/com/shinnlove/wechatpay/service/RequestForPicture.java
+++ src/main/java/com/shinnlove/wechatpay/service/RequestForPicture.java
@@ -4,22 +4,17 @@
  */
 package com.shinnlove.wechatpay.service;
 
-import java.io.*;
-import java.net.MalformedURLException;
-import java.net.URLEncoder;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.apache.http.HttpEntity;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
-import com.shinnlove.wechatpay.http.diy.PoolingHttpClient;
 
 /**
  * 利用JSoup请求图片。
@@ -34,60 +29,9 @@ public class RequestForPicture {
     public static void main(String[] args) {
         List<String> images = getImages();
         for (String url : images) {
-            executor.submit(() -> downImages("./miko/", url));
+            executor.submit(() -> RequestForImages.downImages(url, "./miko/"));
         }
         executor.shutdown();
-    }
-
-    /**
-     * 下载图片到指定目录
-     *
-     * @param filePath 文件路径
-     * @param imgUrl   图片URL
-     */
-    public static void downImages(String filePath, String imgUrl) {
-        // 若指定文件夹没有，则先创建
-        File dir = new File(filePath);
-        if (!dir.exists()) {
-            dir.mkdirs();
-        }
-        // 截取图片文件名
-        String fileName = imgUrl.substring(imgUrl.lastIndexOf('/') + 1, imgUrl.length());
-
-        try {
-            // 文件名里面可能有中文或者空格，所以这里要进行处理。但空格又会被URLEncoder转义为加号
-            String urlTail = URLEncoder.encode(fileName, "UTF-8");
-            // 因此要将加号转化为UTF-8格式的%20
-            imgUrl = imgUrl.substring(0, imgUrl.lastIndexOf('/') + 1)
-                     + urlTail.replaceAll("\\+", "\\%20");
-
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        // 写出的路径
-        File file = new File(filePath + File.separator + fileName);
-
-        try {
-            // http文件流
-            HttpEntity entity = PoolingHttpClient.getImagesDownload(imgUrl);
-            InputStream in = entity.getContent();
-
-            // 获得文件输出流
-            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file));
-            // 构建缓冲区(1KB得读写)
-            byte[] buf = new byte[1024];
-            int size;
-            // 写入到文件
-            while (-1 != (size = in.read(buf))) {
-                out.write(buf, 0, size);
-            }
-            out.close();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
     }
 
     /**
